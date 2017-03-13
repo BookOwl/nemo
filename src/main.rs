c93c889ce572bddd5fccc52e39285d00d00dab57
BookOwl <stanleybookowl@gmail.com>
2017-03-13 19:21:42 -0400

    Changed main to parse expressions inputed and display the result

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate nemo;
+
+use std::io::{stdin, stdout, Read, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut stdin = stdin();
+    let mut stdout = stdout();
+    loop {
+        print!("> ");
+        stdout.flush();
+        let mut input = String::new();
+        stdin.read_line(&mut input);
+        let parsed = nemo::parser::parse_Definition(&input);
+        if parsed.is_ok() {
+            println!("{:?}", parsed.unwrap());
+        } else {
+            let parsed = nemo::parser::parse_Expr(&input);
+            if parsed.is_ok() {
+                println!("{:?}", parsed.unwrap());
+            } else {
+                let parsed = nemo::parser::parse_Statement(&input);
+                println!("{:?}", parsed.unwrap());
+            }
+        }
+    }
 }
