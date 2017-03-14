c805f2bbff481c97928d69ec63ddd56eed97c53c
BookOwl <stanleybookowl@gmail.com>
2017-03-14 12:45:26 -0400

    Add basic REPL

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,27 @@ extern crate nemo;
 use std::io::{stdin, stdout, Write};
 
 fn main() {
+    expression_repl();
+}
+
+fn expression_repl() {
+    let stdin = stdin();
+    let mut stdout = stdout();
+    println!("><> nemo v0.0.1 <><");
+    println!("Use Ctrl-C to exit.");
+    loop {
+        print!("> ");
+        stdout.flush().unwrap();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        match nemo::interpreter::eval(&input) {
+            Ok(res) => println!("{}", res),
+            Err(e)  => println!("Error: {:?}", e),
+        }
+    }
+}
+
+fn parse_repl() {
     let stdin = stdin();
     let mut stdout = stdout();
     println!("><> nemo v0.0.1 <><");
