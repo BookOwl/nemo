5b349f9868a34fea696c88fc9c05c85c4b77065f
BookOwl <stanleybookowl@gmail.com>
2017-03-15 18:11:28 -0400

    Remove parsing REPL code

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use std::error::Error;
 
 fn main() {
     expression_repl();
-    //parse_repl()
 }
 
 fn expression_repl() {
@@ -31,25 +30,5 @@ fn expression_repl() {
             Ok(res) => println!("{}", res),
             Err(e)  => println!("Error: {:?}", e),
         };
-    }
-}
-
-fn parse_repl() {
-    let stdin = stdin();
-    let mut stdout = stdout();
-    println!("><> nemo v0.0.1 <><");
-    println!("Use Ctrl-C to exit.");
-    loop {
-        print!("> ");
-        stdout.flush().unwrap();
-        let mut input = String::new();
-        stdin.read_line(&mut input).unwrap();
-        let parsed = nemo::parser::parse_Definition(&input);
-        if parsed.is_ok() {
-            println!("{:?}", parsed.unwrap());
-        } else {
-            let parsed = nemo::parser::parse_Expr(&input);
-            println!("{:?}", parsed.unwrap());
-        }
     }
 }
